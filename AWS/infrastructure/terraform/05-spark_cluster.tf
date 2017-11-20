e1583d952706fdd68698b5ab8d9529f753946656
Dirk Fuchs <difu@mac.com>
2017-11-20 17:19:19 +0100

    Cluster is installed correctly

--- AWS/infrastructure/terraform/05-spark_cluster.tf
+++ AWS/infrastructure/terraform/05-spark_cluster.tf
@@ -6,8 +6,10 @@ resource "aws_emr_cluster" "arcus-emr-cluster" {
   ec2_attributes {
     subnet_id                         = "${aws_subnet.public-a.id}"
     instance_profile                  = "EMR_EC2_DefaultRole"
-    additional_master_security_groups = "${aws_security_group.arcus-public-ssh.id}"
-    additional_slave_security_groups  = "${aws_security_group.arcus-public-ssh.id}"
+    emr_managed_master_security_group = "${aws_security_group.arcus_emr_master_sg.id}"
+    emr_managed_slave_security_group  = "${aws_security_group.arcus_emr_slave_sg.id}"
+    additional_master_security_groups = "${aws_security_group.arcus-public-ssh.id},${aws_security_group.arcus-public-ssl.id}"
+    additional_slave_security_groups  = "${aws_security_group.arcus-public-ssh.id},${aws_security_group.arcus-public-ssl.id}"
     key_name = "${var.key_name}"
   }
 
@@ -20,13 +22,62 @@ resource "aws_emr_cluster" "arcus-emr-cluster" {
     name     = "arcus-emr"
   }
 
-  bootstrap_action {
+/*  bootstrap_action {
     path = "s3://elasticmapreduce/bootstrap-actions/run-if"
     name = "runif"
     args = ["instance.isMaster=true", "echo running on master node"]
-  }
+  }*/
 
   configurations = "test-fixtures/emr_configurations.json"
 
   service_role = "${aws_iam_role.iam_emr_service_role.arn}"
+}
+
+resource "aws_security_group" "arcus_emr_master_sg" {
+  name        = "arcus_emr_master_sg"
+  description = "To be used with AWS managed ingress/egress rules"
+  vpc_id      = "${aws_vpc.arcus.id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    ignore_changes = ["ingress", "egress"]
+  }
+
+  revoke_rules_on_delete = "true"
+  depends_on = ["aws_subnet.public-a"]
+
+
+  tags {
+    Name = "arcus_emr_master_sg"
+  }
+}
+
+resource "aws_security_group" "arcus_emr_slave_sg" {
+  name        = "arcus_emr_slave_sg"
+  description = "To be used with AWS managed ingress/egress rules"
+  vpc_id      = "${aws_vpc.arcus.id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    ignore_changes = ["ingress", "egress"]
+  }
+
+  revoke_rules_on_delete = "true"
+  depends_on = ["aws_subnet.public-a"]
+
+  tags {
+    Name = "arcus_emr_slave_sg"
+  }
 }

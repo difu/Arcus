42ef3f2509c06211bd372d91d06934e0f615dbc8
Dirk Fuchs <difu@mac.com>
2018-11-04 11:27:55 +0100

    ssh between subnets

--- AWS/infrastructure/terraform/01-vpc.tf
+++ AWS/infrastructure/terraform/01-vpc.tf
@@ -115,7 +115,12 @@ resource "aws_security_group" "arcus-public-ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   tags {
     name    = "${var.project} Public SSH"
     project = "${var.project}"

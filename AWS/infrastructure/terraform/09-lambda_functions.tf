bacd933bdfb200b121c4bb1ef1b8ecb8b2cbcbb9
Dirk Fuchs <difu@mac.com>
2018-11-18 22:41:21 +0100

    fixed refactoring

--- AWS/infrastructure/terraform/09-lambda_functions.tf
+++ AWS/infrastructure/terraform/09-lambda_functions.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_function" "lambda_grib2geotiff" {
   function_name = "ServerlessExample"
-  count = "${var.deploy_lambda_convert_grib_geolambda}"
+  count = "${var.deploy_lambda_convert_grib2geotiff}"
   s3_bucket = "${var.arcus_internal_bucket_name}"
   s3_key    = "lambda/lambda_grib2geotiff.zip"
 

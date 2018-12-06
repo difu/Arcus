5c0a19ae8cfd7cc0d90bc01727c4d0a807fb6126
Dirk Fuchs <difu@mac.com>
2018-12-06 22:15:38 +0100

    comment for importing and removing resource

--- AWS/infrastructure/terraform/00-root.tf
+++ AWS/infrastructure/terraform/00-root.tf
@@ -1,3 +1,6 @@
 resource "aws_s3_bucket" "internal_bucket" {
   # (resource arguments)
 }
+
+# terraform import aws_s3_bucket.internal_bucket devel-arcus-internal
+# terraform state rm aws_s3_bucket.internal_bucket

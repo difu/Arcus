cc1753175a0d834c92a46ba578ad9f95264dde43
difu <difu@me.com>
2018-12-19 15:31:26 +0100

    Use deployment package in S3 instead of local zip

--- AWS/infrastructure/terraform/09-lambda_functions.tf
+++ AWS/infrastructure/terraform/09-lambda_functions.tf
@@ -74,12 +74,13 @@ data "archive_file" "lambda_api_gw_test" {
 }
 
 resource "aws_lambda_function" "test_api_gw-lambda" {
-  filename = "${data.archive_file.lambda_api_gw_test.output_path}"
-  function_name = "test_api_gw-lambda"
+  s3_bucket = "${var.arcus_internal_bucket_name}"
+  s3_key    = "lambda/lambda_gdal.zip"
+  function_name = "rasterblaster"
   role = "${aws_iam_role.lambda_exec.arn}"
   handler = "lambda_function.lambda_handler"
   runtime = "python3.6"
-  source_code_hash = "${base64sha256(file(data.archive_file.lambda_api_gw_test.output_path))}"
+  #source_code_hash = "${base64sha256(file(data.archive_file.lambda_api_gw_test.output_path))}"
 }
 
 resource "aws_api_gateway_rest_api" "rasterblaster" {
